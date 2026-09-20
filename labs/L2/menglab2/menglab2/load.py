470c7b2ecd29e95a2b3826d5a31e9f369e57cec3
Andrew L Ferguson <andrew.l.ferguson@gmail.com>
2026-09-20 08:29:42 -0500

    Corrected L2 local url

--- labs/L2/menglab2/menglab2/load.py
+++ labs/L2/menglab2/menglab2/load.py
@@ -105,7 +105,7 @@ def load_full_dataset(save_csv: bool = False) -> pd.DataFrame:
     # ============================================================
     #url = "https://cran.r-project.org/src/contrib/rcdk_3.8.1.tar.gz"
     #url = "https://cran.r-project.org/src/contrib/Archive/rcdk/rcdk_3.8.1.tar.gz"
-    url = "https://github.com/andrewlferguson/MENG15100/blob/main/labs/L2/rcdk_3.8.1.tar.gz"
+    url = "https://github.com/andrewlferguson/MENG15100/raw/refs/heads/main/labs/L2/rcdk_3.8.1.tar.gz"
     resp = requests.get(url, timeout=60)
     resp.raise_for_status()
 
